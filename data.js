6fe39f4f19ca34c24e20e29824800a9d2e422939
pvvideos <provider04officiall@gmail.com>
2026-01-30 23:39:44 +0530

    Update data.js
    Signed-off-by: pvvideos <provider04officiall@gmail.com>

--- data.js
+++ data.js
@@ -2,26 +2,26 @@ const posters = [
   {
     id: 1,
     title: "Instagram chashmish viral girl video",
-    image: "IMG_20260130_165312_974.jpg",
+    image: "IMG_20260130_233522.jpg",
     description: "Most trending Instagram viral video",
-    terabox: "https://terabox-link-1",
-    telegram: "https://telegram-link-1"
+    terabox: "https://1024terabox.com/s/1DUlspvTp2HZCayEj-kh9hQ",
+    telegram: "https://earning-boot-provider-04.in/movies-here"
   },
   {
     id: 2,
     title: "19 minute 34 second viral video full HD",
     image: "images/IMG_20260130_171412_225.jpg",
     description: "Full HD viral couple video",
-    terabox: "https://terabox-link-2",
-    telegram: "https://telegram-link-2"
+    terabox: "https://teraboxurl.com/s/12LEPgspYBPW8cZ8_os81xg",
+    telegram: "https://earning-boot-provider-04.in/movies-here"
   },
   {
     id: 3,
     title: "Third viral post demo",
     image: "ChatGPT Image Jan 30, 2026, 09_33_30 PM.png",
     description: "Third post test",
-    terabox: "https://terabox-link-3",
-    telegram: "https://telegram-link-3"
+    terabox: "https://1024terabox.com/s/187vXBKbj8zZKqaXdGsFK-A",
+    telegram: "https://earning-boot-provider-04.in/movies-here"
   },
   {
     id: 4,
