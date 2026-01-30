6d668cdbf4cf4618b88e29e5ce2d228da98d525e
pvvideos <provider04officiall@gmail.com>
2026-01-30 20:44:09 +0530

    Update data.js
    Signed-off-by: pvvideos <provider04officiall@gmail.com>

--- data.js
+++ data.js
@@ -1,10 +1,18 @@
-var posters = [
+const posters = [
   {
     id: 1,
-    title: "Test Movie One",
+    title: "instgram chashmish viral girl video",
     image: "images/IMG_20260130_165312_974.jpg",
-    description: "Test description",
-    terabox: "https://example.com",
-    telegram: "https://t.me/example"
+    description: "most trading viral Instagram video",
+    terabox: "https://1024terabox.com/s/1DUlspvTp2HZCayEj-kh9hQ",
+    telegram: "https://earning-boot-provider-04.in/movies-here"
+  },
+  {
+    id: 2,
+    title: "19 minute 34 second viral video full hd",
+    image: "images/IMG_20260130_171412_225.jpg",
+    description: "Instagram most viral video",
+    terabox: "https://teraboxurl.com/s/12LEPgspYBPW8cZ8_os81xg",
+    telegram: "https://earning-boot-provider-04.in/movies-here"
   }
 ];
