37bf5ab8886ed571d8af2ccdbe68444d1903428d
pvvideos <provider04officiall@gmail.com>
2026-01-30 23:05:58 +0530

    Create data.js
    Signed-off-by: pvvideos <provider04officiall@gmail.com>

---
+++ data.js
@@ -0,0 +1,26 @@
+const posters = [
+  {
+    id: 1,
+    title: "Instagram chashmish viral girl video",
+    image: "images/poster1.jpg",
+    description: "Most trending Instagram viral video",
+    terabox: "https://terabox-link-1",
+    telegram: "https://telegram-link-1"
+  },
+  {
+    id: 2,
+    title: "19 minute 34 second viral video full HD",
+    image: "images/poster2.jpg",
+    description: "Full HD viral couple video",
+    terabox: "https://terabox-link-2",
+    telegram: "https://telegram-link-2"
+  },
+  {
+    id: 3,
+    title: "Third viral post demo",
+    image: "images/poster3.jpg",
+    description: "Third post test",
+    terabox: "https://terabox-link-3",
+    telegram: "https://telegram-link-3"
+  }
+];
