f88ad5329007218f144b76a33516ee918b099b50
pvvideos <provider04officiall@gmail.com>
2026-01-30 20:28:07 +0530

    Update script.js
    Signed-off-by: pvvideos <provider04officiall@gmail.com>

--- script.js
+++ script.js
@@ -1,16 +1,21 @@
-alert("JS WORKING");
+document.addEventListener("DOMContentLoaded", function () {
+  alert("JS LOADED");
 
-const container = document.getElementById("poster-container");
+  const container = document.getElementById("poster-container");
+  if (!container) {
+    alert("Container nahi mila");
+    return;
+  }
 
-posters.forEach(item => {
-  const box = document.createElement("a");
-  box.href = "details.html?id=" + item.id;
-  box.className = "poster";
+  posters.forEach(function (item) {
+    const box = document.createElement("a");
+    box.href = "details.html?id=" + item.id;
+    box.className = "poster";
 
-  box.innerHTML = `
-    <img src="${item.image}">
-    <h3>${item.title}</h3>
-  `;
+    box.innerHTML =
+      '<img src="' + item.image + '">' +
+      '<h3>' + item.title + '</h3>';
 
-  container.appendChild(box);
+    container.appendChild(box);
+  });
 });
