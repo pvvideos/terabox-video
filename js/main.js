55675db18859a58758aff4a5ee8ddbd94c47708d
pvvideos <provider04officiall@gmail.com>
2026-01-30 22:29:33 +0530

    Update main.js
    Signed-off-by: pvvideos <provider04officiall@gmail.com>

--- js/main.js
+++ js/main.js
@@ -1,1 +1,18 @@
+const grid = document.getElementById("poster-container");
+
+posters.forEach(item => {
+  const card = document.createElement("div");
+  card.className = "card";
+
+  card.innerHTML = `
+    <img src="${item.image}">
+    <h3>${item.title}</h3>
+  `;
+
+  card.onclick = () => {
+    window.location.href = `details.html?id=${item.id}`;
+  };
+
+  grid.appendChild(card);
+});
 
