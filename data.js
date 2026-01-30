6f49e38639a828ea2e2340ab3060e868fc6d77da
pvvideos <provider04officiall@gmail.com>
2026-01-30 19:58:54 +0530

    Update data.js
    Signed-off-by: pvvideos <provider04officiall@gmail.com>

--- data.js
+++ data.js
@@ -1,11 +1,11 @@
 const posters = [
   {
     id: 1,
-    title: "Movie One",
-    image: "images/poster1.jpg",
-    description: "Full movie description here",
-    terabox: "https://teraboxlink",
-    telegram: "https://t.me/yourchannel"
+    title: "instgram chashmish viral girl video",
+    image: "images/IMG_20260130_165312_974.jpg",
+    description: " most trading viral Instagram video chashmish girl is here click here to watch full videos",
+    terabox: "https://1024terabox.com/s/1DUlspvTp2HZCayEj-kh9hQ",
+    telegram: "https://earning-boot-provider-04.in/movies-here"
   },
   {
     id: 2,
